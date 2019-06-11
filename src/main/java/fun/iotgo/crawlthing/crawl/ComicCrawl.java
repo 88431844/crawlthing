9f6d899a480837c6e5f59f027a890231ed8f22a6
luck <88431844@163.com>
2019-06-11 09:49:19 +0800

    增加流程

--- src/main/java/fun/iotgo/crawlthing/crawl/ComicCrawl.java
+++ src/main/java/fun/iotgo/crawlthing/crawl/ComicCrawl.java
@@ -17,6 +17,13 @@ public class ComicCrawl {
   }
 
   public static void main(String[] args) {
+    /**
+     * 1.通过漫画URL，获取该漫画每个章节的链接列表
+     * 2.通过每个章节的链接，获取该章节总共页数
+     * 3.拼接 章节URL+page=N 组成每一话链接
+     * 4.下载每一话图片以 漫画名称/章节 结构存储图片
+     * 5.压缩下载后的漫画，并改后缀名为cbz
+     */
     ComicCrawl.getComic();
   }
 }
